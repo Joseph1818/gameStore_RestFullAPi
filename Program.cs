946bc71234163ca9520a47cdc22c1c3768e9f90c
umbaJ <joseph@dwebserver.net>
2025-02-12 13:17:07 +0200

    delete  method added

--- Program.cs
+++ Program.cs
@@ -63,4 +63,11 @@ app.MapPut("games/{id}", (int id, UpdateGameDto updateGameDto)  => {
 
 });
 
+// DELETE/Game
+app.MapDelete("games/{id}",( int id ) => {
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 app.Run();
